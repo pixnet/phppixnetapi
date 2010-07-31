d36b8a9e29b559df3cf3dbcfe8822790c74e4dc5
ronnywang@gmail.com <ronnywang@gmail.com@fe195f98-7436-3a42-412e-c0440e11308f>
2010-07-31 15:12:26 +0000

    修改 sample2
    git-svn-id: https://phppixnetapi.googlecode.com/svn/trunk@12 fe195f98-7436-3a42-412e-c0440e11308f

--- sample2.php
+++ sample2.php
@@ -16,4 +16,12 @@ $api = new PixAPI('__CONSUMER_KEY__', '__CONSUMER_KEY__');
 $api->setToken($_GET['request_token'], $_SESSION['request_token_' . $_GET['request_token']);
 list($access_token, $access_token_secret) = $api->getAccessToken($_GET['verifier_token']);
 // 將 $access_token 和 $access_token_secret 記錄起來，以後就直接使用 $api->setToken($access_token, $access_token_secret) 就可以作 API 的動作了。
+$_SESSION['access_token'] = $access_token;
+$_SESSION['access_token_secret'] = $access_token_secret;
+
+/**
+ * IN http://mysite.com/pixnet/getinfo
+ */
+$api = new PixAPI('__CONSUMER_KEY__', '__CONSUMER_KEY__');
+$api->setToken($_SESSION['access_token'], $_SESSION['access_token_secret']);
 print_r($api->user_get_account());

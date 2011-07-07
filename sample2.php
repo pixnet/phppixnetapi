4b343c4f12cb0cf8241b1fa113783029e0ba3900
David <mu574n6@pixnet.tw>
2011-07-07 17:47:25 +0800

    補上]

--- sample2.php
+++ sample2.php
@@ -13,7 +13,7 @@ header('Location: ' . $api->getAuthURL('http://mysite.com/pixnet/return'));
  * IN http://mysite.com/pixnet/return
  */
 $api = new PixAPI('__CONSUMER_KEY__', '__CONSUMER_KEY__');
-$api->setToken($_GET['request_token'], $_SESSION['request_token_' . $_GET['request_token']);
+$api->setToken($_GET['request_token'], $_SESSION['request_token_' . $_GET['request_token']]);
 list($access_token, $access_token_secret) = $api->getAccessToken($_GET['verifier_token']);
 // 將 $access_token 和 $access_token_secret 記錄起來，以後就直接使用 $api->setToken($access_token, $access_token_secret) 就可以作 API 的動作了。
 $_SESSION['access_token'] = $access_token;

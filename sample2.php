3908b229e1606359f39d9960304b7a97f6e7c6f3
Repeat Yang <repeat@pixnet.tw>
2012-01-04 15:14:17 +0800

    修正 sample2 中 PixAPI 的參數，第二個應該填 SECRET

--- sample2.php
+++ sample2.php
@@ -3,7 +3,7 @@
 /**
  * IN http://mysite.com/pixnet/start
  */
-$api = new PixAPI('__CONSUMER_KEY__', '__CONSUMER_KEY__');
+$api = new PixAPI('__CONSUMER_KEY__', '__CONSUMER_SECRET__');
 list($request_token, $request_token_secret) = $api->getRequestTokenPair();
 // 記錄起來 $request_token & $request_token_secret
 $_SESSION['request_token_' . $request_token] = $request_token_secret;
@@ -12,7 +12,7 @@ header('Location: ' . $api->getAuthURL('http://mysite.com/pixnet/return'));
 /**
  * IN http://mysite.com/pixnet/return
  */
-$api = new PixAPI('__CONSUMER_KEY__', '__CONSUMER_KEY__');
+$api = new PixAPI('__CONSUMER_KEY__', '__CONSUMER_SECRET__');
 $api->setToken($_GET['oauth_token'], $_SESSION['request_token_' . $_GET['oauth_token']]);
 list($access_token, $access_token_secret) = $api->getAccessToken($_GET['oauth_verifier']);
 // 將 $access_token 和 $access_token_secret 記錄起來，以後就直接使用 $api->setToken($access_token, $access_token_secret) 就可以作 API 的動作了。
@@ -22,6 +22,6 @@ $_SESSION['access_token_secret'] = $access_token_secret;
 /**
  * IN http://mysite.com/pixnet/getinfo
  */
-$api = new PixAPI('__CONSUMER_KEY__', '__CONSUMER_KEY__');
+$api = new PixAPI('__CONSUMER_KEY__', '__CONSUMER_SECRET__');
 $api->setToken($_SESSION['access_token'], $_SESSION['access_token_secret']);
 print_r($api->user_get_account());
